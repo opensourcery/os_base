7a3eddddeb8e3cf8cf28d659702fc29bce3bcd40
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2010-09-28 16:35:25 -0700

    Rename class to "dropdown-blocks" and add a region preprocessor to set it.

--- os_base.js
+++ os_base.js
@@ -1,12 +1,13 @@
+// Adapted from Open Atrium: http://openatrium.com
 Drupal.behaviors.osBase = function (context) {
-  // Togglable blocks
-  $('div.toggle-blocks:not(.processed)')
+  // Drop-down blocks
+  $('div.dropdown-blocks:not(.processed)')
     .addClass('processed')
     .each(function() {
       $('.block-title', this).click(function() {
         if (!$(this).is('.toggle-active')) {
-          $('div.toggle-blocks .block-title').removeClass('toggle-active');
-          $('div.toggle-blocks div.block-toggle div.block-content').hide();
+          $('div.dropdown-blocks .block-title').removeClass('toggle-active');
+          $('div.dropdown-blocks div.block-toggle div.block-content').hide();
           $(this).addClass('toggle-active').siblings('div.block-content').show();
         }
         else {
