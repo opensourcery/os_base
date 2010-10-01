bf5983286c79eb6b581365439995ebe352f78876
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2010-10-01 16:27:15 -0700

    Focus first form input element of drop-down blocks.

--- os_base.js
+++ os_base.js
@@ -4,11 +4,16 @@ Drupal.behaviors.osBase = function (context) {
   $('div.dropdown-blocks:not(.processed)')
     .addClass('processed')
     .each(function() {
-      $('.block-title', this).click(function() {
+      $('.block-title', this).click(function(event) {
+        // don't travel to the link location, if any
+        event.preventDefault();
         if (!$(this).is('.toggle-active')) {
           $('div.dropdown-blocks .block-title').removeClass('toggle-active');
           $('div.dropdown-blocks div.block-toggle div.block-content').hide();
           $(this).addClass('toggle-active').siblings('div.block-content').show();
+
+          // if there's a form inside our drop-down, focus its first input
+          $(this).siblings('div.block-content').find('input:first').focus();
         }
         else {
           $(this).removeClass('toggle-active').siblings('div.block-content').hide();
