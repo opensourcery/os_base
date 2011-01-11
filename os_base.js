a1fad5b29136868920b09aa52276c797fea01eb0
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2011-01-10 18:25:31 -0800

    Update JavaScript to work with new (standard block) markup format for dropdowns.

--- os_base.js
+++ os_base.js
@@ -1,24 +1,29 @@
-// Adapted from Open Atrium: http://openatrium.com
-Drupal.behaviors.osBase = function (context) {
-  // Drop-down blocks
-  $('div.dropdown-blocks:not(.processed)')
-    .addClass('processed')
-    .each(function() {
-      $('.block-title', this).click(function(event) {
-        // don't travel to the link location, if any
-        event.preventDefault();
-        if (!$(this).is('.toggle-active')) {
-          $('div.dropdown-blocks .block-title').removeClass('toggle-active');
-          $('div.dropdown-blocks div.block-toggle div.block-content').hide();
-          $(this).addClass('toggle-active').siblings('div.block-content').show();
+;(function ($) {
+  // Adapted from Open Atrium: http://openatrium.com
+  Drupal.behaviors.osBase = function (context) {
+    // Drop-down blocks
+    $('.dropdown-block:not(.processed)')
+      .addClass('processed')
+      .each(function() {
+        $('.title', this).click(function(event) {
+          // don't travel to the link location, if any
+          event.preventDefault();
+          if (!$(this).is('.toggle-active')) {
+            // Deactivate all drop down blocks
+            $('.dropdown-block .title').removeClass('toggle-active');
+            $('.dropdown-block .content').hide();
 
-          // if there's a form inside our drop-down, focus its first input
-          $(this).siblings('div.block-content').find('input:first').focus();
-        }
-        else {
-          $(this).removeClass('toggle-active').siblings('div.block-content').hide();
-        }
-        return false;
+            // Activate this dropdown
+            $(this).addClass('toggle-active').siblings('.content').show();
+
+            // if there's a form inside our drop-down, focus its first input
+            $(this).siblings('.content').find('input:first').focus();
+          }
+          else {
+            $(this).removeClass('toggle-active').siblings('.content').hide();
+          }
+          return false;
+        });
       });
-    });
-};
+  };
+})(jQuery);
